62e7c7a6cbb6659d6ca92d5ccbd7577532a8842c
grace335 <67076065+grace335@users.noreply.github.com>
2020-07-12 22:27:51 -0400

    Update create_COVIDx_v3.py

--- create_COVIDx_v3.py
+++ create_COVIDx_v3.py
@@ -1,6 +1,8 @@
 # Converted from COVID-Net create_COVIDx_V3.ipynb
 # Source: https://github.com/lindawangg/COVID-Net/blob/master/create_COVIDx_v3.ipynb
 
+#!/usr/bin/env python3
+
 import numpy as np
 import pandas as pd
 import os
